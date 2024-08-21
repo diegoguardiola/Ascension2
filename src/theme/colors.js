025d7578e780b630e643b75ded2c68615dbf51c0
diegoguardiola-falco <diego.g@falcoautomation.com>
2024-08-21 02:25:00 -0400

    first commit

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+
+    background: '#fff',
+    text: '#000',
+    grey: '#31393C'
+}
+
+export default Colors
---
+++ src/theme/size.js
@@ -0,0 +1,13 @@
+
+const Size = {
+    normal: 12,
+    large: 16,
+    small: 9
+}
+
+const Weight = {
+    normal: 'normal',
+    bold: 'bold'
+}
+
+export default {Size, Weight}
